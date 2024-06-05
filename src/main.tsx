7639b8ac7dda92062d99e559d01bb838b93b80bd
Andres Briñez <brinezlopez08@gmail.com>
2024-06-05 14:38:53 -0500

    Initial project setup: Configure styles and layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import { App } from './app';
 import ReactDOM from 'react-dom/client';
+import './app/ui/styles/style.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode />,
+  <App />
 );
