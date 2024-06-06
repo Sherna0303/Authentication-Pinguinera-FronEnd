2fb0c055e71dea8a037a9aee4e7c485b8c98a005
Andres Briñez <brinezlopez08@gmail.com>
2024-06-05 22:41:19 -0500

    Partially implement handleSubmit for login validation

--- src/app/ui/components/Login/index.tsx
+++ src/app/ui/components/Login/index.tsx
@@ -1,20 +1,42 @@
-import { ReactElement } from 'react';
+import { ReactElement, useContext } from 'react';
 import './style.css';
-import { FormLogin } from '../../forms/login';
+import FormLogin from '../../forms/login';
+import { AppContext } from '../../../core/state/AppContext';
+import Swal from 'sweetalert2';
 
 const LoginComponent = (): ReactElement => {
   // const [email, setEmail] = useState('');
   // const [password, setPassword] = useState('');
   // // const { authenticate, errorMessage } = useAuthLogin();
+  const { state } = useContext(AppContext);
 
-  // const handleSubmit = (event: { preventDefault: () => void; }) => {
-  //   event.preventDefault();
-  //   // authenticate(email, password);
-  // };
+
+  const handleSubmit = async (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+
+    const emailUser = state.email;
+    const passwordUser = state.password;
+
+    if (emailUser.length<1 || passwordUser.length<1) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Campos Incorrectos',
+        text: 'Verifica que los dato ingresados sean los correctos',
+      });
+      return;      return;
+    }
+    Swal.fire({
+      icon: 'success',
+      title: 'Formulario enviado',
+      text: 'El formulario se ha enviado correctamente.',
+    });
+
+  };
+
 
   return (
     <main className="auth authLogin">
-      <FormLogin />
+      <FormLogin handleSubmit={handleSubmit} />
     </main>
   );
 };
