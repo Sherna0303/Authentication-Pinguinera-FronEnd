7b20a8e3494f92cdfb7dd69605d82498d2f4f84a
Andres Briñez <brinezlopez08@gmail.com>
2024-06-05 21:53:11 -0500

    Add form validations and error styling

--- src/app/ui/forms/login/index.tsx
+++ src/app/ui/forms/login/index.tsx
@@ -1,30 +1,25 @@
 import { Link } from 'react-router-dom';
-import { FormEvent, ReactElement, useState } from 'react';
 import { Label } from '../../elements/Label';
 import './style.css';
+import { useState } from 'react';
 
-export const FormLogin = (): ReactElement => {
+interface LoginProps {
+  handleSubmit?: (e: React.FormEvent<HTMLFormElement>) => void;
+}
+
+const FormLogin: React.FC<LoginProps> = ({ handleSubmit }: LoginProps) => {
 
   const [email, setEmail] = useState<string>('');
-  const [emailError, setEmailError] = useState<string | null>(null);
   const [password, setPassword] = useState<string>('');
-  const [passwordError, setPasswordError] = useState<string | null>(null);
-
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    alert('Enviando formulario');
-    // if (!username || !email || !password || !repeatPassword || password.length < 8 || password !== repeatPassword || username.length < 3 || !isValidEmail(email)) {
-    //   return;
-    // }
-  };
+  const [error, setError] = useState<string>('');
 
   const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newEmail = event.target.value;
     setEmail(newEmail);
     if (!isValidEmail(email)) {
-      setEmailError('Introduzca una dirección de correo electrónico válida');
+      setError('Introduzca una dirección de correo electrónico válida');
     } else {
-      setEmailError(null);
+      setError('');
     }
   };
 
@@ -36,52 +31,53 @@ export const FormLogin = (): ReactElement => {
     const newPassword = event.target.value;
     setPassword(newPassword);
     if (password.length < 1) {
-      setPasswordError('La contraseña no puede estar vacía');
+      setError('La contraseña no puede estar vacía');
     }
     if (password.length < 8) {
-      setPasswordError('La contraseña debe tener al menos 8 caracteres');
+      setError('La contraseña debe tener al menos 8 caracteres');
     } else {
-      setPasswordError(null);
+      setError('');
     }
   };
 
 
   return (
-    <main className="auth authLogin">
-      <form onSubmit={handleSubmit} className="auth__form">
-  
-        <fieldset className="auth__fieldset">
+    <form onSubmit={handleSubmit} className="auth__form">
 
-          <legend className="auth__legend">Iniciar sesión</legend>
+      <fieldset className="auth__fieldset">
 
-          <Label classNameLabel='auth__label'
-            classNameSpan='auth__span'
-            classNameInput={`auth__input ${emailError ? 'error' : ''}`}
-            nameInput='name' 
-            type='email'
-            text='Correo electronico'
-            placeHolder='Ingresa tu correo electrónico'
-            onChange={handleEmailChange}
-          />
+        <legend className="auth__legend">Iniciar sesión</legend>
 
-          <Label classNameLabel='auth__label'
-            classNameSpan='auth__span'
-            classNameInput={`auth__input ${passwordError ? 'error' : ''}`}
-            nameInput='name'
-            type='password'
-            text='Contraseña'
-            placeHolder='Ingresa tu contraseña'
-            onChange={handlePasswordChange}
-          />
-          
-          <button className="auth__button auth__button--primary" type="submit">
-            Iniciar sesión
-          </button>
+        <Label classNameLabel='auth__label'
+          classNameSpan='auth__span'
+          classNameInput={'auth__input'}
+          nameInput='name'
+          type='email'
+          text='Correo electronico'
+          placeHolder='Ingresa tu correo electrónico'
+          onChange={handleEmailChange}
+        />
 
-          <p>¿No tienes una cuenta? <Link to='/register' className="auth__register-link">Regístrate</Link></p>
+        <Label classNameLabel='auth__label'
+          classNameSpan='auth__span'
+          classNameInput={'auth__input'}
+          nameInput='name'
+          type='password'
+          text='Contraseña'
+          placeHolder='Ingresa tu contraseña'
+          onChange={handlePasswordChange}
+        />
 
-        </fieldset>
-      </form>
-    </main>
+        <button className="auth__button auth__button--primary" type="submit">
+          Iniciar sesión
+        </button>
+
+        <p>¿No tienes una cuenta? <Link to='/register' className="auth__register-link">Regístrate</Link></p>
+        {error && <p className="auth__error">{error}</p>}
+      </fieldset>
+    </form>
   );
 };
+
+
+export default FormLogin;
