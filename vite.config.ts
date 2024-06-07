2960819326acd368bab27ee5011ce853ef4d959d
Sebastian_Hernandez_Patio <sebastianhernandezpatio.2004@gmail.com>
2024-06-07 09:32:21 -0500

    Fix deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Authentication-Pinguinera-FronEnd/'
+  base: 'https://sherna0303.github.io/Authentication-Pinguinera-FronEnd/'
 });
