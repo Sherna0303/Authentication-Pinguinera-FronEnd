4813bfdd32b836a9cf0f50534d187a64f59a8f49
Sebastian_Hernandez_Patio <sebastianhernandezpatio.2004@gmail.com>
2024-06-07 10:55:11 -0500

    Add auto-direction to login

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthLayout } from '../ui/layouts/auth-layout';
 import { RegisterPage } from '../pages/register.page';
 import { LoginPage } from '../pages/login.page';
@@ -8,6 +8,7 @@ export const AppRouter = () => {
     <HashRouter>
       <Routes>
         <Route path="/" element={<AuthLayout />}>
+          <Route index element={<Navigate to={'/login'}/>}/>
           <Route path="login" element={<LoginPage />} />
           <Route path="register" element={<RegisterPage />} />
         </Route>
